b1aff26ad8bdbbfee65d149e348169d7bd4c0cc3
Liviu-Gabriel Zbant <zbant.liviu.gabriel@gmail.com>
2021-01-15 10:33:01 +0200

    Initial commit

---
+++ src/test/java/com/spring/autosales/AutosalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.autosales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutosalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
